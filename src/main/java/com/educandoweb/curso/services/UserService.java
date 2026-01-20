bef6553c4b60a4d945c3289da081484da6c211e0
Lucas <lucasdalazen06@gmail.com>
2026-01-20 20:50:28 -0300

    layer de serviço e registro de componentes

---
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> op = userRepository.findById(id);
+		return op.get();
+	}
+}
