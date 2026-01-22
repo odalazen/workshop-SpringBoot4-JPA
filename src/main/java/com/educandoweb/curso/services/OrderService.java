5213d06c740762dbf973dcf0ea92797a82101df0
Lucas <lucasdalazen06@gmail.com>
2026-01-21 21:30:30 -0300

    datas com instant e padrao ISO

---
+++ src/main/java/com/educandoweb/curso/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.curso.entities.Order;
+import com.educandoweb.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> op = orderRepository.findById(id);
+		return op.get();
+	}
+}
